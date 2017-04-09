fa66396a1b7cea1cdd079afb50888610173eb069
Gaffrs <colmgaffney@yahoo.ie>
2017-04-09 18:30:15 +0100

    Unit Tests added

--- BikeHireUnitTest/UnitTest1.cs
+++ BikeHireUnitTest/UnitTest1.cs
@@ -1,22 +1,141 @@
-﻿using System;
+﻿// Create Unit Tests:                    Add New Project - Test - Unit Test Project
+// Need to add a reference to link it:   Right click the UnitTest File - Add - Reference - Project - Select the Project
+// How to run the Unit Tests:            Test - Run - All Tests
+// Check for Code Coverage:              Test - Analyze Code Coverage for all tests
+// Debug                                 Test - Debug - All tests
+
+// Assert Class https://msdn.microsoft.com/en-us/library/ms182530.aspx 
+// Assert
+// Collection Assert
+// String Assert
+
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using BikeHire.Models; //required to access the models
+using System.ComponentModel.DataAnnotations; //required in order to use ValidationException
 
 namespace BikeHireUnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTesting
     {
         [TestMethod]
-        public void TestMethod1()
+        public void HireRentalDaysTest()
         {
-            HireDetailsDto b = new HireDetailsDto();
+            // arrange  
+            Hire b = new Hire();
+            // act 
             b.StartDate = new DateTime(2017, 03, 01);
             b.FinishDate = new DateTime(2017, 03, 11);
+            // assert
+            Assert.AreEqual(b.RentalDays, 10);
+        }
 
-            Assert.AreEqual(b.RentalDays, 11);
+        [TestMethod]
+        public void HireFieldsTest()
+        {
+            // arrange           
+            Hire b = new Hire();
+            // act 
+            b.HireID = 1;
+            b.FirstName = "firstname";
+            b.Surname = "surname";
+            b.Address = "dublin";
+            b.PhoneNumber = "122222";
+            b.StartDate = new DateTime(2017, 03, 01);
+            b.FinishDate = new DateTime(2017, 03, 11);
+            // assert
+            Assert.AreEqual(b.HireID, (1));
+            Assert.AreEqual(b.FirstName, ("firstname"));
+            Assert.AreEqual(b.Surname, ("surname"));
+            Assert.AreEqual(b.Address, ("dublin"));
+            Assert.AreEqual(b.PhoneNumber, ("122222"));
+            Assert.AreEqual(b.StartDate, (new DateTime(2017, 03, 01)));
+            Assert.AreEqual(b.FinishDate, (new DateTime(2017, 03, 11)));
+        }
+
+        [TestMethod]
+        public void BikeFieldsTest()
+        {
+            // arrange           
+            Bike b = new Bike();
+            // act 
+            b.BikeID = 1;
+            b.Make = "name";
+            b.Model = "surname";
+            b.RentalChargePerDay = 25;
+            b.BikeAvailable = false;
+            // assert
+            Assert.AreEqual(b.BikeID, (1));
+            Assert.AreEqual(b.Make, ("name"));
+            Assert.AreEqual(b.Model, ("surname"));
+            Assert.AreEqual(b.RentalChargePerDay, (25));
+            Assert.AreEqual(b.BikeAvailable, (false));
 
         }
+
+        /*
+                [TestMethod]
+                [ExpectedException(typeof(ArgumentException))]
+                public void HireFields1()
+                {
+                    // arrange 
+                    Hire b = new Hire();
+                    // act 
+                    b.FirstName = "";
+                    b.Surname = "";
+                    // assert
+
+                }
+                */
+
+        /*
+                [TestMethod()]
+                [ExpectedException(typeof(ValidationAttribute))]
+                public void FieldsCorrect()
+
+                {
+                    // arrange 
+                    Hire b = new Hire();
+                    b.FirstName = "";
+                    b.Surname = "";
+                    b.Address = "";
+                    b.PhoneNumber = "";
+                    // act 
+                    // assert
+                    //Assert.
+                    //Assert.Equals("required")
+
+                }
+                */
+
+        /*
+                [TestMethod]
+                [ExpectedException(typeof(ValidationException), "Required field")]
+                [ExpectedException(typeof(ArgumentException),
+
+                public void FieldsCorrect()
+                {
+                    // arrange 
+                    Hire b = new Hire();
+                    b.FirstName = "";
+                    b.Surname = "";
+                    b.Address = "";
+                    b.PhoneNumber = "";
+                    // act 
+                    // assert
+                    AssertFailedException.Equals("required")
+
+                    MyAssert.Throws<NullReferenceException>(() => testStr.ToUpper());
+
+
+
+                }
+            }
+
+            }*/
+
     }
 }
