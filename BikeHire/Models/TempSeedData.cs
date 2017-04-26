c790cab3d2f2d8ff48cfc93a90f1bc16f53099d7
Gaffrs <colmgaffney@yahoo.ie>
2017-04-26 21:29:54 +0100

    Created a TempSeedData Model to store Seed data.

---
+++ BikeHire/Models/TempSeedData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BikeHire.Models
+{
+    public class TempSeedData
+    {
+        /*
+                context.Bikes.AddOrUpdate(p => p.BikeID,
+                        new Models.Bike { Make = "Trek", Model = "Madone", RentalChargePerDay = 35, BikeAvailable = false },
+                        new Models.Bike { Make = "Giant", Model = "Defy", RentalChargePerDay = 20, BikeAvailable = true },
+                        new Models.Bike { Make = "Boardman", Model = "Carbon", RentalChargePerDay = 15, BikeAvailable = true },
+                        new Models.Bike { Make = "Raleigh", Model = "Grifter", RentalChargePerDay = 5, BikeAvailable = true },
+                        new Models.Bike { Make = "Giant", Model = "Defy Advanced 2", RentalChargePerDay = 30, BikeAvailable = true },
+                        new Models.Bike { Make = "Giant", Model = "Defy Advanced 1", RentalChargePerDay = 70, BikeAvailable = true },
+                        new Models.Bike { Make = "Cannondale", Model = "Slate", RentalChargePerDay = 20, BikeAvailable = true }
+                        );
+
+                    context.Hires.AddOrUpdate(r => r.HireID,
+                        new Models.Hire { BikeID = 1, FirstName = "Colm", Surname = "Gaffney", Address = "Dublin 15", PhoneNumber = "0871234567", StartDate = new DateTime(2017, 03, 01), FinishDate = new DateTime(2017, 03, 03) },
+                        new Models.Hire { BikeID = 1, FirstName = "John", Surname = "Gaffney", Address = "Dublin 10", PhoneNumber = "0871235678", StartDate = new DateTime(2017, 04, 01), FinishDate = new DateTime(2017, 04, 10) },
+                        new Models.Hire { BikeID = 1, FirstName = "Aaron", Surname = "O'Leary", Address = "Dublin 1", PhoneNumber = "0871236666", StartDate = new DateTime(2017, 05, 01), FinishDate = new DateTime(2017, 05, 03) },
+                        new Models.Hire { BikeID = 2, FirstName = "John", Surname = "Swan", Address = "Cork", PhoneNumber = "0871234555", StartDate = new DateTime(2017, 05, 01), FinishDate = new DateTime(2017, 05, 10) },
+                        new Models.Hire { BikeID = 1, FirstName = "Mark", Surname = "Doran", Address = "Meath", PhoneNumber = "0123344444", StartDate = new DateTime(2017, 05, 07), FinishDate = new DateTime(2017, 05, 09) }
+                        );
+        */
+    }
+}
