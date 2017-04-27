ed91a8092d4520601adfdaf41b1cd91d9156f7d8
Gaffrs <colmgaffney@yahoo.ie>
2017-04-27 18:58:13 +0100

    Code update

--- BikeHire/Migrations/Configuration.cs
+++ BikeHire/Migrations/Configuration.cs
@@ -28,7 +28,7 @@ namespace BikeHire.Migrations
             //    );
             //
 
-
+/*  //Seed Data
             context.Hires.AddOrUpdate(r => r.HireID,
                 new Models.Hire { BikeID = 1, FirstName = "Colm", Surname = "Gaffney", Address = "Dublin 15", PhoneNumber = "0871234567", StartDate = new DateTime(2017, 03, 01), FinishDate = new DateTime(2017, 03, 03) },
                 new Models.Hire { BikeID = 1, FirstName = "John", Surname = "Gaffney", Address = "Dublin 10", PhoneNumber = "0871235678", StartDate = new DateTime(2017, 04, 01), FinishDate = new DateTime(2017, 04, 10) },
@@ -38,14 +38,15 @@ namespace BikeHire.Migrations
                 );
 
             context.Bikes.AddOrUpdate(p => p.BikeID,
-                //new Models.Bike { Make = "Trek", Model = "Madone", RentalChargePerDay = 35, BikeAvailable = false },
-                //new Models.Bike { Make = "Giant", Model = "Defy", RentalChargePerDay = 20, BikeAvailable = true },
-                //new Models.Bike { Make = "Boardman", Model = "Carbon", RentalChargePerDay = 15, BikeAvailable = true },
-                new Models.Bike { Make = "Raleigh", Model = "Grifter", RentalChargePerDay = 5, BikeAvailable = true },
+                new Models.Bike { Make = "Trek", Model = "Madone", RentalChargePerDay = 35, BikeAvailable = false },
+                new Models.Bike { Make = "Giant", Model = "Defy", RentalChargePerDay = 20, BikeAvailable = true },
+                new Models.Bike { Make = "Boardman", Model = "Carbon", RentalChargePerDay = 15, BikeAvailable = true },
+                new Models.Bike { Make = "Raleigh", Model = "Grifter", RentalChargePerDay = 5, BikeAvailable = false },
                 new Models.Bike { Make = "Giant", Model = "Defy Advanced 2", RentalChargePerDay = 30, BikeAvailable = true },
                 new Models.Bike { Make = "Giant", Model = "Defy Advanced 1", RentalChargePerDay = 70, BikeAvailable = true },
                 new Models.Bike { Make = "Cannondale", Model = "Slate", RentalChargePerDay = 20, BikeAvailable = true }
         );
+*/
 
 
 
